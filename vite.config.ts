29b6e7f7b24de121a6da2c8ca818b4917a65f4eb
Tsuteto <midis.cube@gmail.com>
2023-11-16 21:01:57 +0900

    Fixed config and rebuilt

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     vueJsx(),
@@ -17,5 +18,8 @@ export default defineConfig({
   },
   server: {
     port: 3000
+  },
+  build: {
+    outDir: "docs"
   }
 })
